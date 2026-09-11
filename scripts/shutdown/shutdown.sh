b9536a4c75c7d32d968ee9abcfebad901132d978
The1lama <87243876+The1lama@users.noreply.github.com>
2026-09-11 22:03:10 +0200

    [feat] Shutdown screen: ssn gives a shutdown screen
    Terminal alias ssn (sudo shutdown now) displays an image before shuting
    down.
    
    ---

---
+++ scripts/shutdown/shutdown.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+working_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+images_path="${working_dir}/shutdown"
+
+## Save the files in the array $files
+files=("$images_path"/*.jpg)
+# Get a random file from path
+rand_file="${files[RANDOM % ${#files[@]}]}"
+
+conf_path="${images_path}/shutdownImage.conf"
+
+# create a hyprlock conf file for displaying wallpaper 
+# and echo the config in to file
+touch $conf_path
+echo "background {
+    monitor =
+    path = ${rand_file}
+    blur_passes = 0
+}" > $conf_path 
+
+# run the hyprlock and then shutdown the computer
+hyprlock -c "${conf_path}" &
+
+# sleep for n secons
+sleep 5
+
+# shutdown
+shutdown -h now 
+
+
+
